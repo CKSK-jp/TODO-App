bab7fdb6665e710492ead8b6f7da0b9c21dfd2c6
cksk-jp <carlson.ksk@gmail.com>
2023-11-14 11:10:37 -0500

    Completed localStorage functionality

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function renderTasks() {
   tasks.forEach(function (taskObject) {
     const newTask = document.createElement('li');
     const textContainer = document.createElement('span');
+
     // store taskInput into span element and add appropriate class names
     textContainer.innerText = taskObject.text;
     textContainer.classList.add('todoText');
@@ -22,13 +23,18 @@ function renderTasks() {
 
     // create the complete and remove task buttons
     const completionBtn = document.createElement('button');
-    const removeBtn = document.createElement('button');
-    completionBtn.classList.add('completionBtn')
-    removeBtn.classList.add('removeBtn')
     completionBtn.innerText = 'completed';
+    const removeBtn = document.createElement('button');
     removeBtn.innerText = 'x';
-    // append them to the task
-    newTask.append(completionBtn, " ", removeBtn);
+    completionBtn.classList.add('completionBtn');
+    removeBtn.classList.add('removeBtn');
+
+    if (taskObject.completed) {
+      textContainer.classList.add('completed-task');
+      newTask.append(removeBtn);
+    } else {
+      newTask.append(completionBtn, " ", removeBtn);
+    }
   });
 }
 
@@ -42,7 +48,7 @@ form.addEventListener('submit', function (e) {
   if (taskInput.value === '') {
     alert('Your task cannot be empty!');
   } else {
-    // create a new task object
+    // create a new task object with text and completed properties
     const taskObject = {
       text: taskInput.value,
       completed: false,
@@ -51,7 +57,7 @@ form.addEventListener('submit', function (e) {
     // add task to task object array
     tasks.push(taskObject);
     // store tasks into local storage
-    localStorage.setItem('task', JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     // update new tasks to render
     renderTasks();
     taskInput.value = '';
@@ -60,15 +66,31 @@ form.addEventListener('submit', function (e) {
 
 // strike out the task if complete has been clicked
 todoList.addEventListener('click', function (e) {
-  if (e.target.innerText === 'completed') {
-    const doneTODO = e.target.parentElement.querySelector('.todoText');
+  let targettedTask = e.target;
+  let doneTODO = targettedTask.parentElement.querySelector('span');
+
+  if (targettedTask.innerText === 'completed') {
     doneTODO.classList.add('completed-task'); // this line will add the strike out css class
-    e.target.remove();
+    // now we want to check our tasks and find the task clicked on.
+    const taskObject = tasks.find(function(task) {
+      return task.text === doneTODO.innerText;
+    });
+    // set completed property to true and store
+    taskObject.completed = true;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    targettedTask.remove();
 
     // if the x button is clicked, remove it
   } else if (e.target.innerText === 'x') {
-    e.target.parentElement.remove();
+    targettedTask.parentElement.remove();
+    //find the index of the removed task and remove it from localStorage
+    const taskIndex = tasks.findIndex(function(task) {
+      return task.text === doneTODO.innerText;
+    });
+
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
   }
 })
-
-
