6920934d580105a01be31b49b4e0f975dfedf944
cksk-jp <carlson.ksk@gmail.com>
2023-11-14 13:03:52 -0500

    Remove consolelogs, better comments

--- app.js
+++ app.js
@@ -2,49 +2,53 @@ const taskInput = document.querySelector('#task-input');
 const form = document.querySelector('#todo-form');
 const todoList = document.querySelector('#todo-list')
 
-// Retrieve any tasks from localStorage on page load
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+// Retrieve any tasks from localStorage on page load, error if invalid JSON
+try {
+  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+} catch (error) {
+  console.error('Error parsing tasks from localStorage:', error);
+}
 
-//Handle actions after user submits a new task
+// Handle actions after user submits a new task
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  // check if entered value is not empty, print error message if so
+  // Check if entered value is not empty, print error message if so
   if (taskInput.value === '') {
     alert('Your task cannot be empty!');
   } else {
-    // create a new task object with text and completed properties
+    // Create a new task object with properties, text & completed
     const taskObject = {
       text: taskInput.value,
       completed: false,
     };
 
-    // add task to task object array
+    // add this new task object to our tasks array and store it into localStorage
     tasks.push(taskObject);
-    // store tasks into local storage
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    // update new tasks to render
+    
+    // Render tasks and reset user input
     renderTasks();
     taskInput.value = '';
   }
-})
+});
 
 // Render tasks onto page
 function renderTasks() {
   todoList.innerHTML = ''; // clear any existing tasks
   
-  // build each task
+  // Build each task
   tasks.forEach(function (taskObject) {
     const newTask = document.createElement('li');
     const textContainer = document.createElement('span');
 
-    // store taskInput into span element and add appropriate class names
+    // Store taskInput into span element and add appropriate class names
     textContainer.innerText = taskObject.text;
     textContainer.classList.add('todoText');
     newTask.classList.add('todo-task');
     newTask.appendChild(textContainer);
     todoList.appendChild(newTask);
 
-    // create the complete and remove task buttons
+    // Create the complete and remove task buttons
     const completionBtn = document.createElement('button');
     completionBtn.innerText = 'completed';
     const removeBtn = document.createElement('button');
@@ -52,6 +56,7 @@ function renderTasks() {
     completionBtn.classList.add('completionBtn');
     removeBtn.classList.add('removeBtn');
 
+    // Check if a stored task has been completed, if so load the page with that class and the corresponding class to strike out the text, otherwise append both buttons to task.
     if (taskObject.completed) {
       textContainer.classList.add('completed-task');
       newTask.append(removeBtn);
@@ -64,34 +69,34 @@ function renderTasks() {
 // Initialize render tasks on page load
 renderTasks();
 
-
-// strike out the task if complete has been clicked
+// Strike out the task if complete has been clicked
 todoList.addEventListener('click', function (e) {
   let targettedTask = e.target;
   let doneTODO = targettedTask.parentElement.querySelector('span');
 
   if (targettedTask.innerText === 'completed') {
-    doneTODO.classList.add('completed-task'); // this line will add the strike out css class
-    // now we want to check our tasks and find the task clicked on.
+    doneTODO.classList.add('completed-task');
+    // Now we want to check our tasks and find the task clicked on.
     const taskObject = tasks.find(function(task) {
       return task.text === doneTODO.innerText;
     });
-    // set completed property to true and store
+    // Set completed property to true and store
     taskObject.completed = true;
     localStorage.setItem('tasks', JSON.stringify(tasks));
     targettedTask.remove();
 
-    // if the x button is clicked, remove it
+    // If the x button is clicked, remove it
   } else if (e.target.innerText === 'x') {
     targettedTask.parentElement.remove();
-    //find the index of the removed task and remove it from localStorage
+    // Find the index of the removed task and remove it from localStorage
     const taskIndex = tasks.findIndex(function(task) {
       return task.text === doneTODO.innerText;
     });
 
+    // Check if findIndex gets the matching array, remove queried index from tasks. Update to sync tasks with this removal change
     if (taskIndex !== -1) {
       tasks.splice(taskIndex, 1);
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
   }
-})
+});
